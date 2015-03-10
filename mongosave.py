ce0393de6e185de060fb80904194b99c9d384d5a
amitt001 <mail2amit19@gmail.com>
2015-03-10 16:08:09 +0530

    added login/signup. Multiple pads

--- mongosave.py
+++ mongosave.py
@@ -1,4 +1,5 @@
 import pymongo
+from bson.objectid import ObjectId
 from datetime import datetime
 
 class mongosave:
@@ -10,16 +11,51 @@ class mongosave:
 
 	def __init__(self):
 		self.connection = pymongo.Connection()
-		self.db = self.connection['pastepad_db']
-		self.collection = self.db['paste_collection']
+		self.db = self.connection['codebuddy_db']
+		#self.collection = self.db['paste_collection']
+
+	def userlogin(self, email, password):
+		#self.collection = self.db['user_login']
+		self.collection = self.db['testdb']
+		try:
+			result = list(self.collection.find({'user_email': email}))
+			if not len(result):
+				print('########new######')
+				result = str(self.collection.insert({'user_email': email, 'user_password': password}))
+			else:
+				if result[0].get('user_password') == password:
+					result = str(result[0].get('_id'))
+					print("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
+					print('####already#####', result)
+				else:
+					print('####already#####')
+					return -2
+
+			return result
+
+		except Exception, e:
+			print(e)			
+			return -1
+
+	def login_check(self, _id):
+		self.collection = self.db['testdb']
+		result = list(self.collection.find({'_id': ObjectId(_id)}))
+		print str(result[0].get('_id'))
+		if str(result[0].get('_id')) == _id:
+			return 1
+		else: 
+			return -1
+
 
 	def save(self, _id, data):
+		self.collection = self.db['paste_collection']
 		currdate = datetime.now().strftime('%Y-%m-%d')
 		currtime = datetime.now().strftime('%H:%M')
 		self.collection.insert({'_id': _id, 'paste_doc': data, "date": currdate, 'time': currtime})
 		print(list(self.collection.find()))
 
 	def check(self, _id):
+		self.collection = self.db['paste_collection']
 		try:
 			result = list(self.collection.find({'_id': _id}, {'paste_doc':1, '_id':0}))
 			if not len(result):
--- views.py
+++ views.py
@@ -3,7 +3,7 @@ monkey.patch_all()
 
 import time
 from threading import Thread
-from flask import Flask, render_template, session, request, redirect, url_for
+from flask import Flask, render_template, session, request, redirect, url_for, session
 from flask.ext.socketio import SocketIO, emit
 import random
 import string
@@ -15,7 +15,7 @@ app.debug = True
 app.config['SECRET_KEY'] = 'shhhh...'
 socketio = SocketIO(app)
 thread = None
-dict1 = {}
+masterdict = {}
 codepadlst = [None] * 2   # for maintaining the rooms in codepad
 count = 0
 uniqid = 'random'
@@ -25,9 +25,44 @@ uniqid = 'random'
 def about():
     return render_template('about.html')
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':        
+        if request.form.get('login', None) == 'Sign Up/Login':
+
+            user_email = request.form.get('email', None)
+            user_pass = request.form.get('password', None)
+
+            if len(user_email) > 5 and len(user_pass) > 5:
+                dbclass = mongosave.mongosave()
+                session_id = dbclass.userlogin(user_email, user_pass)
+                if (session_id == -1):
+                    return 'ERROR: RELOAD'
+                if (session_id == -2):
+                    return 'User already exists and wrong password for this email'
+                session['id'] = session_id
+                return redirect(url_for('index'))
+            else:
+                return render_template('login.html')
+    return render_template('login.html')
+
+@app.route('/logout', methods=['GET', 'POST'])
+def logout():
+    session.pop('useremail', None)
+    session.pop('password', None)
+    return redirect(url_for('index'))
+
 @app.route('/publicpad/<padid>') #unique url
 def publicpad(padid):
-    return render_template('newpad.html')
+    session_id = session.get('id', 0)
+    if not session_id:
+        return redirect(url_for('login'))
+    dbclass = mongosave.mongosave()
+    valid_check = dbclass.login_check(session_id)
+    if valid_check == -1:
+        return "Please login again"
+    else:
+        return render_template('newpad.html', sessionid=session_id)
 
 
 @app.route('/docpad/<padid>')
@@ -52,11 +87,11 @@ def index():
     if request.method == 'POST':
 
         if request.form.get('public', None) == 'New Public Codepad':
-            codepadurl =             uniquelink = ''.join(random.choice(string.ascii_letters+string.digits) for i in range(9))
+            codepadurl = ''.join(random.choice(string.ascii_letters+string.digits) for i in range(9))
             return redirect(url_for('publicpad', padid=codepadurl))
 
         if request.form.get('docpad', None) == 'New Document Pad':
-            docpadurl =             uniquelink = ''.join(random.choice(string.ascii_letters+string.digits) for i in range(8))
+            docpadurl = ''.join(random.choice(string.ascii_letters+string.digits) for i in range(8))
             return redirect(url_for('textpad', padid=docpadurl))
 
         if request.form.get('paste', None) == 'Paste Pad':
@@ -88,6 +123,23 @@ def remove_id(message):
 def sync(message):
     #doing all the checks for only two connection allowable
     #########This is not executed for 3rd pad. look into it#########
+    data = masterdict.get(message['id'])
+    session_id = session['id']
+    if not data:
+        masterdict[message['id']] = []
+        masterdict[message['id']].append(session_id)
+        emit('sync response', {'data': message['data'], 'id': session_id, 'url': message['id']}, broadcast=True)
+    else:
+        if len(data) < 2 and session_id not in data:
+            masterdict[message['id']].append(session_id)
+            emit('sync response', {'data': message['data'], 'id': session_id, 'url': message['id']}, broadcast=True)            
+            print 'MasterDict right now ', masterdict
+        if session_id in data:
+            emit('sync response', {'data': message['data'], 'id': session_id, 'url': message['id']}, broadcast=True)
+        elif len(data)>=2 and session_id not in data:
+            emit('error response', {'error': 'CodePad already in use. Ask creater to allow you. Thank you.', 'block': 'yes'})
+
+def fool():
     if codepadlst[0] == None and message['id'] not in codepadlst:
         codepadlst[0] = message['id']
     elif codepadlst[1] == None and message['id'] not in codepadlst:
